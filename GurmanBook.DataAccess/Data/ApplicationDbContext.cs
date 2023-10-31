c1d497474cdf063ef4d49e682d82a24c4907f354
gurman2912 <w0830963@myscc.ca>
2023-10-31 19:25:02 -0400

    install .Identity.EntityFrameworkCore and modify namespace in ApplicationDbContext.cs

--- GurmanBook.DataAccess/Data/ApplicationDbContext.cs
+++ GurmanBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace GurmanBooksStore.Data
+namespace GurmanBooksStore.DataAccess.Data //modify namspace to reflect the project
 {
     public class ApplicationDbContext : IdentityDbContext
     {
