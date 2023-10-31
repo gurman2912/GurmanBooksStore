ce7c27c870f19965c4e23146ac96ccb98987d26e
gurman2912 <w0830963@myscc.ca>
2023-10-31 12:06:51 -0400

    create project and make changes on line 35 in Startup.cs file

--- GurmanBooksStore/Startup.cs
+++ GurmanBooksStore/Startup.cs
@@ -32,7 +32,7 @@ namespace GurmanBooksStore
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()//(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
