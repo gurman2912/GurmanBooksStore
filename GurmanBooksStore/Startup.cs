9c0a5bf8cf20eb5ed2938b19bad4163d3a1f0860
gurman2912 <w0830963@myscc.ca>
2023-11-02 17:50:36 -0400

    modify Startup.cs service 'addContext' with using statement

--- GurmanBooksStore/Startup.cs
+++ GurmanBooksStore/Startup.cs
@@ -1,4 +1,4 @@
-using GurmanBooksStore.Data;
+using GurmanBooksStore.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
