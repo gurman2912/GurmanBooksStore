7a4348af4662e7c7588fa63b9dfb19ab665c4eae
gurman2912 <w0830963@myscc.ca>
2023-11-06 15:06:00 -0500

    add Repository folder to .DataAccess under this add IRepository folder and under this folder add new item named as IRepository.cs and modify it

---
+++ GurmanBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace GurmanBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);  //Retrieve a category from the database by id
+        // List of Categories based on requriements
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null    //useful of foreign key references
+            );
+        void Add(T entity); // to add an entity
+
+        void Remove(int id); //to remove an object or category
+
+        void Remove(T entity); //another way to remove an object
+
+        void RemoveRange(IEnumerable<T> entity); //removes a complete range of entities
+    }
+}
