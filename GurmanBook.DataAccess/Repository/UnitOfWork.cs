8d9e202346c345e43c641d75c85575dae4a395fa
gurman2912 <w0830963@myscc.ca>
2023-11-06 22:21:03 -0500

    add UnitOfWork.cs class to Repository and implement the interface

---
+++ GurmanBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using GurmanBook.DataAccess.Repository.IRepository;
+using GurmanBooksStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GurmanBook.DataAccess.Repository
+{
+    public class UnitOfWork     //make the method public to access the class
+    {
+        private readonly ApplicationDbContext _db; //the using statement
+
+        public UnitOfWork(ApplicationDbContext db)   //constructor to use DI and inject in to the repositories
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()  //all changes will be saved when the save method is called at the 'parent' level
+        {
+            _db.SaveChanges();
+        }
+    }
+}
