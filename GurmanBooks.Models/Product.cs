605a67036c68419e349213927e38ca864e30ff81
gurman2912 <w0830963@myscc.ca>
2023-11-20 21:38:27 -0500

    create Product class to .Model and add data

---
+++ GurmanBooks.Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace GurmanBooks.Models
+{
+   public class Product
+    {
+        [Key]
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public string Description { get; set;}
+        public string ISBN { get; set;}
+        public string Author { get; set;}
+        [Required]
+        [Range(1,10000)]
+        public double ListPrice { get; set;}
+        public string ImageUrl { get; set;}
+
+        [Required]
+        public int CategoryId { get; set; }//Foreign Key reference
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set;}
+
+        [Required]
+        public int CoverId { get; set; }  //Foreign Key reference
+        [ForeignKey("CoverId")]
+        public CoverType CoverType { get; set;}
+
+
+    }
+}
