e5666da7b83c67df3cee4d2e8abd61b221cd5976
gurman2912 <w0830963@myscc.ca>
2023-11-20 22:07:54 -0500

    apply Required method on "Title", "ISBN" and "Author", create add migration and update database

--- GurmanBook.DataAccess/Migrations/20231121030413_AddValidationToProduct.Designer.cs
+++ GurmanBook.DataAccess/Migrations/20231121030413_AddValidationToProduct.Designer.cs
@@ -4,14 +4,16 @@ using GurmanBooksStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GurmanBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121030413_AddValidationToProduct")]
+    partial class AddValidationToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
